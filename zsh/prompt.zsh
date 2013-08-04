a4554d5e95431ce9dc5718231d3e5d8cb88bcc63
Michael Bumann <michael@railslove.com>
2013-08-04 20:40:37 +0200

    remove todo from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -39,52 +39,6 @@ need_push () {
 }
 
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo_count(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls $1 | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
-function todo_prompt() {
-  local COUNT=$(todo_count $1);
-  if [ "$COUNT" != 0 ]; then
-    echo "$1: $COUNT";
-  else
-    echo "";
-  fi
-}
-
-function notes_count() {
-  if [[ -z $1 ]]; then
-    local NOTES_PATTERN="TODO|FIXME|HACK";
-  else
-    local NOTES_PATTERN=$1;
-  fi
-  grep -ERn "\b($NOTES_PATTERN)\b" {app,config,lib,spec,test} 2>/dev/null | wc -l | sed 's/ //g'
-}
-
-function notes_prompt() {
-  	local COUNT=$(notes_count $1);
-  	if [ "$COUNT" != 0 ]; then
-    		echo "$1: $COUNT";
-  	else
-    		echo "";
-  	fi
-}
-
 
 directory_name(){
   echo "%{$fg_bold[cyan]%}%2/%\/%{$reset_color%}"
@@ -99,5 +53,4 @@ set_prompt () {
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
 }
