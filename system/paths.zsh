358e16e132a3673a4d4ecee3e590eac47962c44d
Michael Bumann <hello@michaelbumann.com>
2017-03-25 17:06:00 +0630

    init pyenv

--- system/paths.zsh
+++ system/paths.zsh
@@ -2,6 +2,9 @@ PATH=".bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 eval "$(nodenv init -)"
 eval "$(rbenv init -)"
+eval "$(pyenv init -)"
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
 
 export PATH
 
