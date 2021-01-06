9377fa7382c5813d21915e1e6ccf0eaa71bd55dc
Michael Bumann <hello@michaelbumann.com>
2021-01-06 11:22:41 +0100

    Update

--- system/paths.zsh
+++ system/paths.zsh
@@ -1,19 +1,20 @@
 PATH=".bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
-eval "$(nodenv init -)"
-eval "$(rbenv init -)"
-eval "$(pyenv init -)"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
 export GOPATH=$HOME/src/go
 PATH=$PATH:$GOPATH/bin
 
 PATH=$PATH:./node_modules/.bin:$HOME/.node_modules/bin
+PATH=$PATH:$HOME/.nodenv/bin
 
 PATH=$PATH:$HOME/.cargo/bin
 
 # install node modules locally
 export npm_config_prefix=$HOME/.node_modules
+
+eval "$(nodenv init -)"
+eval "$(rbenv init -)"
+eval "$(pyenv init -)"
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
 export PATH
 
